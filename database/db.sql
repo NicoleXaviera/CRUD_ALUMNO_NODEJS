72cf10b4bf3e4c825826958fda48e82acc1ca1c9
NicoleXaviera <nicole.arguedas@tecsup.edu.pe>
2023-09-27 23:44:38 -0500

    segundo commit

--- database/db.sql
+++ database/db.sql
@@ -1,19 +1,13 @@
--- to create a new database
-CREATE DATABASE customersdb;
+CREATE DATABASE crudnodejs;
 
--- to use database
-use customersdb;
+USE crudnodejs;
 
--- creating a new table
-CREATE TABLE customer (
+CREATE TABLE student (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
   phone VARCHAR(15)
 );
 
--- to show all tables
-show tables;
 
--- to describe table
-describe customer;
+SELECT * FROM student;
--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "crudnodejs.cqv1do4jd2wh.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Tecsup2023",
   port: 3306,
   database: "crudnodejs",
 });
